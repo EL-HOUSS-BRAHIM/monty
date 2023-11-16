cf7ef74cf9566044bb5d0a043aa7b1664580723b
EL-HOUSS-BRAHIM <87819687+EL-HOUSS-BRAHIM@users.noreply.github.com>
2023-11-16 17:21:50 +0100

    Update monty_main.c

--- monty_main.c
+++ monty_main.c
@@ -1,50 +1,56 @@
 #include "monty.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+void push(Stack *stack, int value, int line_number) {
+    if (stack->top == STACK_SIZE - 1) {
+        fprintf(stderr, "L%d: stack overflow\n", line_number);
         exit(EXIT_FAILURE);
     }
+    stack->top++;
+    stack->stack[stack->top] = value;
+}
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+int pop(Stack *stack, int line_number) {
+    if (stack->top == -1) {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
+    return stack->stack[stack->top--];
+}
 
-    Stack stack;
-    stack.top = -1;
-
-    char opcode[100];
-    int value, line_number = 0;
-
-    while (fscanf(file, "%s", opcode) != EOF) {
-        line_number++;
-        if (strcmp(opcode, "push") == 0) {
-            if (fscanf(file, "%d", &value) != 1) {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            push(&stack, value, line_number);
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall(&stack);
-        } else if (strcmp(opcode, "pint") == 0) {
-            pint(&stack, line_number);
-        } else if (strcmp(opcode, "pop") == 0) {
-            pop(&stack, line_number);
-        } else if (strcmp(opcode, "swap") == 0) {
-            swap(&stack, line_number);
-        } else if (strcmp(opcode, "add") == 0) {
-            add(&stack, line_number);
-        } else if (strcmp(opcode, "nop") == 0) {
-            nop();
-        } else {
-            fprintf(stderr, "L%d: unknown opcode %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
+void pall(Stack *stack) {
+    int i;
+    for (i = stack->top; i >= 0; i--) {
+        printf("%d\n", stack->stack[i]);
     }
+}
 
-    fclose(file);
+void pint(Stack *stack, int line_number) {
+    if (stack->top == -1) {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", stack->stack[stack->top]);
+}
 
-    return 0;
+void swap(Stack *stack, int line_number) {
+    if (stack->top < 1) {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    int temp = stack->stack[stack->top];
+    stack->stack[stack->top] = stack->stack[stack->top - 1];
+    stack->stack[stack->top - 1] = temp;
+}
+
+void add(Stack *stack, int line_number) {
+    if (stack->top < 1) {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack->stack[stack->top - 1] += stack->stack[stack->top];
+    stack->top--;
+}
+
+void nop() {
+    // Do nothing
 }
